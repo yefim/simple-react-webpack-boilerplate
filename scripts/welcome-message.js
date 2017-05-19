4536742967fca29a70ec8154157e225b33950dee
Yefim Vedernikoff <yefim323@gmail.com>
2017-05-18 18:06:03 -0700

    Add small click demo

--- scripts/welcome-message.js
+++ scripts/welcome-message.js
@@ -2,11 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class WelcomeMessage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      clicks: 0
+    };
+  }
+
   render() {
     const { message } = this.props;
+    const { clicks } = this.state;
 
     return (
-      <h1>{message}</h1>
+      <div>
+        <h1>{message}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              clicks: clicks + 1
+            });
+          }}>Click me</button>
+        <h2>{clicks} click{clicks == 1 ? '' : 's'}</h2>
+      </div>
     );
   }
 }
