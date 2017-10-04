bf533b9115f8185899a51a4b9cb404f9192cf16a
Yefim Vedernikoff <yefim323@gmail.com>
2017-10-04 16:22:44 +0200

    Remove spaces around bracketsz

--- scripts/welcome-message.js
+++ scripts/welcome-message.js
@@ -15,17 +15,15 @@ class WelcomeMessage extends React.Component {
   }
 
   render() {
-    const { message } = this.props;
-    const { clicks } = this.state;
+    const {message} = this.props;
+    const {clicks} = this.state;
 
     return (
       <div>
         <h1>{message}</h1>
         <button
           onClick={() => {
-            this.setState({
-              clicks: clicks + 1
-            });
+            this.setState({clicks: clicks + 1});
           }}>Click me</button>
         <h2>{clicks} click{clicks === 1 ? '' : 's'}</h2>
       </div>
